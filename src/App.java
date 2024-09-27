c0a73a0046258cff11325d90fc8666e4261cd4c0
Jacob4523 <jcgaffke@oakland.edu>
2024-09-26 20:43:48 -0400

    second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String inputStr;
+        String reversedStr = "";
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Input a string that you want to have reversed: ");
+        inputStr = userInput.nextLine();
+        userInput.close();
+
+        for(int i = 0; i < inputStr.length(); i++){
+            reversedStr = inputStr.charAt(i) + reversedStr;
+        }
+
+        System.out.println("\nThe reversed string is: " + reversedStr + "\n");
     }
 }
